6fcc49d71ba9382fde0fdac2db513ac1c96e6270
proyectos.ini.2019 <proyectos.ini.2019@gmail.com>
2019-10-05 22:33:48 -0500

    Actualizando package gitignore

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
  
+const port = process.env.PORT || 3000;
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-console.log("Listening on 3000");
-app.listen(3000)
+
+app.listen(port, () => {
+  console.log("Listening on " +port);
+})
