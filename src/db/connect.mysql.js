331c6f0469c9ce8d405297afdb2d47a6322fac06
proyectos.ini.2019 <proyectos.ini.2019@gmail.com>
2019-10-07 07:18:35 -0500

    Update database config

--- src/db/connect.mysql.js
+++ src/db/connect.mysql.js
@@ -1,6 +1,25 @@
 const Sequelize = require ('sequelize');
 
 const sequelize = new Sequelize(
+    'heroku_29b304c1e6caefe', // DATABASE NAME
+    'b6ca1048637614', // USERNAME
+    '364afa27', // PASSWORD
+    {
+        host:'us-cdbr-iron-east-05.cleardb.net',
+        dialect: 'mysql',
+        pool:{
+            max:5,
+            min: 0,
+            require: 30000,
+            idle: 10000
+        },
+        logging:false
+    }
+)
+
+
+/*
+const sequelize = new Sequelize(
     'miincode',
     'miincode_db_user',
     'miincode_db_pwd',
@@ -16,6 +35,7 @@ const sequelize = new Sequelize(
         logging:false
     }
 )
+*/
 
 var db = {};
 
